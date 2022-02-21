8ee8ba1d6450675312fd70ef45038aa6b0886553
Michael Marlowe <marlowemich@gmail.com>
2022-02-21 15:58:55 -0500

    timer decrements properly. Local storage highscores work

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# codequiz
+This is a quiz layout dealing with a set interval that decrements everytime a an incorrect answer is given. The time left after the six questions becomes the user's score and the user is then prompted to enter their initials which whill be displayed alongside the corresponding score upon reloading the page.
+
+![](2022-02-21-15-57-31.png)
+
+Deployed site here: https://marlowemich.github.io/codequiz/
--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
-
 var body = document.body;
 var contEl = document.createElement("div");
 var startbtn = document.createElement("button");
 var h1El = document.createElement("h1");
 var ulEl = document.createElement("ul");
-
+var scorecont = document.getElementById("pastscores");
+var scorelist = document.createElement("ul");
 
 h1El.textContent = "Code Quiz";
 startbtn.textContent = "startbutton";
@@ -13,8 +13,10 @@ body.appendChild(contEl);
 contEl.appendChild(h1El);
 contEl.appendChild(ulEl);
 contEl.appendChild(startbtn);
-
-//variables of the buttons which will have the possible answer. These shall remain the same in 
+contEl.setAttribute("style", "position: relative; left: 35%;");
+ulEl.setAttribute("style", "display: block;");
+scorecont.appendChild(scorelist);
+//variables of the buttons w; hich will have the possible answer. These shall remain the same in
 //all style attributes but change in the text content
 var a1 = document.createElement("button");
 var a2 = document.createElement("button");
@@ -23,63 +25,115 @@ var a4 = document.createElement("button");
 
 //effects of startbutton
 startbtn.addEventListener("click", function () {
-  console.log("starbutton done got clicked");
-  //countdown();
+  // console.log("starbutton done got clicked");
   contEl.removeChild(startbtn);
   ulEl.appendChild(a1);
+  a1.setAttribute(
+    "style",
+    "padding: 5px; border-radius: 25%; color: white; background-color: purple; display: block;"
+  );
   ulEl.appendChild(a2);
+  a2.setAttribute(
+    "style",
+    "padding: 5px; border-radius: 25%; color: white; background-color: purple; display: block;"
+  );
   ulEl.appendChild(a3);
+  a3.setAttribute(
+    "style",
+    "padding: 5px; border-radius: 25%; color: white; background-color: purple; display: block;"
+  );
   ulEl.appendChild(a4);
+  a4.setAttribute(
+    "style",
+    "padding: 5px; border-radius: 25%; color: white; background-color: purple; display: block;"
+  );
   return getquestionfunc();
-})
+});
 
 //functions from q1 and onward
 
-var questions = [{
-  title: ["question1"],
-  choices: ["1", "2", "3", "4"],
-  answer: ["3"]
-},
-{
-  title: ["question2"],
-  choices: ["a", "b", "c", "d"],
-  answer: ["a"]
-},
-{
-  title: ["question3"],
-  choices: ["!", "_", "-", "%"],
-  answer: ["%"]
-},
-{
-  title: ["question4"],
-  choices: ["11", "22", "33", "44"],
-  answer: ["33"]
-},
-{
-  title: ["question5"],
-  choices: ["6", "7", "8", "9"],
-  answer: ["7"]
-},
-{
-  title: ["question6"],
-  choices: ["A", "Y", "D", "C"],
-  answer: ["D"]
-}]
-var correct = [];
-var incorrect = [];
+var questions = [
+  {
+    title: ["question1"],
+    choices: ["1", "2", "3", "4"],
+    answer: ["3"],
+  },
+  {
+    title: ["question2"],
+    choices: ["a", "b", "c", "d"],
+    answer: ["a"],
+  },
+  {
+    title: ["question3"],
+    choices: ["!", "_", "-", "%"],
+    answer: ["%"],
+  },
+  {
+    title: ["question4"],
+    choices: ["11", "22", "33", "44"],
+    answer: ["33"],
+  },
+  {
+    title: ["question5"],
+    choices: ["6", "7", "8", "9"],
+    answer: ["7"],
+  },
+  {
+    title: ["question6"],
+    choices: ["A", "Y", "D", "C"],
+    answer: ["D"],
+  },
+];
+// var correct = [];
+// var incorrect = [];
 var i = 0;
+let timeLeft = 60;
+var highscores = JSON.parse(localStorage.getItem("scores"))||[];
+
+var s1 = document.createElement("li");
+var s2 = document.createElement("li");
+var s3 = document.createElement("li");
+var s4 = document.createElement("li");
+var s5 = document.createElement("li");
+
+scorelist.appendChild(s1);
+scorelist.appendChild(s2);
+scorelist.appendChild(s3);
+scorelist.appendChild(s4);
+scorelist.appendChild(s5);
+
+s1.textContent = highscores[0];
+s2.textContent = highscores[1];
+s3.textContent = highscores[2];
+s4.textContent = highscores[3];
+s5.textContent = highscores[4];
+
 function getquestionfunc() {
   countdown();
   var timerEl = document.getElementById("timer");
   function countdown() {
-    console.log("timer started");
-    var timeLeft = 60;
+    // console.log("timer started");
     var timeInterval = setInterval(function () {
       timerEl.textContent = "Ye olde seconds remaining: " + timeLeft;
-      //if (decten()){
-      // timerEl.textContent = "Ye olde seconds remaining: " + timeLeft - 10;
-      //}
-      if (timeLeft === -1) {
+      if (timeLeft <= 0){
+          contEl.removeChild(ulEl);
+          h1El.textContent = "SCOREBOARD";
+          var currentscore = document.createElement("h2");
+          contEl.appendChild(currentscore);
+          currentscore.textContent = "Thine score be: " + timeLeft;
+          var initialsprompt = document.createElement("label");
+          contEl.appendChild(initialsprompt);
+          initialsprompt.textContent = "Submit Thy Initials!"
+          var initials = document.createElement("input");
+          contEl.appendChild(initials);
+          var submission = document.createElement("button");
+          contEl.appendChild(submission);
+          submission.textContent = "Submit";
+          submission.addEventListener('click', function(){
+          highscores.push(initials.value + " " + (timeLeft+1));
+          localStorage.setItem("scores", JSON.stringify(highscores))});
+      }
+      if (i===6){
         clearInterval(timeInterval);
         timerEl.remove();
       }
@@ -89,57 +143,62 @@ function getquestionfunc() {
   }
   function questionlist() {
     if (i < questions.length) {
-      //this progresses as q1, q2, q4, q6. why? ANSWER: needed stopPropogation after eventlistener
       h1El.textContent = questions[i].title[0];
       a1.textContent = questions[i].choices[0];
       a2.textContent = questions[i].choices[1];
       a3.textContent = questions[i].choices[2];
       a4.textContent = questions[i].choices[3];
-      console.log(questions[i]);
+      // console.log(questions[i]);
     }
     ulEl.addEventListener("click", function (event) {
       event.stopImmediatePropagation();
       if (event.target.textContent === questions[i].answer[0]) {
         var notif = document.createElement("h2");
         contEl.appendChild(notif);
-        notif.textContent = ("CORRECT!");
-        correct.push("correct");
+        notif.textContent = "CORRECT!";
+        // correct.push("correct");
         setTimeout(correctout);
+
         var correctout = setTimeout(function dissapear() {
           contEl.removeChild(notif);
         }, 750);
-      }
-      else {
+      } else {
+        timerEl.textContent = "Ye olde seconds remaining: " + (timeLeft -= 10);
+          //draw incorrect onto page and decrement time when incorrect
         var notif = document.createElement("h2");
         contEl.appendChild(notif);
-        notif.textContent = ("INCORRECT!");
-        incorrect.push("incorrect");
+        notif.textContent = "INCORRECT!";
+        // incorrect.push("incorrect");
         setTimeout(incorrectout);
+
         var incorrectout = setTimeout(function dissapear() {
           contEl.removeChild(notif);
-          //timerEl.textContent = "Ye olde seconds remaining: " + (timeLeft.value - 10);
-          //return decten();
-          //add event into incorrectout and tie event into timer function to decrement by 10 seconds
         }, 750);
       }
-      //draw incorrect onto page and decrement time when incorrect
       i++;
       if (i === 6) {
         contEl.removeChild(ulEl);
         h1El.textContent = "SCOREBOARD";
-        var wins = document.createElement("h2");
-        var losses = document.createElement("h2");
-        contEl.appendChild(wins);
-        contEl.appendChild(losses);
-        wins.textContent = "Correct: " + correct.length;
-        losses.textContent = "Incorrect: " + incorrect.length;
+        var currentscore = document.createElement("h2");
+        contEl.appendChild(currentscore);
+        currentscore.textContent = "Thine score be: " + timeLeft;
+        var initialsprompt = document.createElement("label");
+        contEl.appendChild(initialsprompt);
+        initialsprompt.textContent = "Submit Thy Initials!"
+        var initials = document.createElement("input");
+        contEl.appendChild(initials);
+        var submission = document.createElement("button");
+        contEl.appendChild(submission);
+        submission.textContent = "Submit";
+    
+        submission.addEventListener('click', function(){
+        highscores.push(initials.value + " " + (timeLeft+1));
+        localStorage.setItem("scores", JSON.stringify(highscores))});
       }
       if (i < 6) {
         return questionlist();
       }
-    })
+    });
   }
+  
 }
-
-
-
